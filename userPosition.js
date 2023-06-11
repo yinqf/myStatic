10d64af8d16b5b7d25b4be937010a3e9eca1d3aa
yinqf <yinqf7437@gmail.com>
2023-06-11 12:41:19 +0800

    语法优化

--- userPosition.js
+++ userPosition.js
@@ -5,8 +5,10 @@ if (url.indexOf('bapi/futures/v5/private/future/user-data/user-position') !== -1
     let dataList = body.data;
     dataList.forEach((item)=>{
         if(item.positionSide === 'LONG' && item.symbol === 'ETHUSDT'){
-            //合约钱包最大可划转金额
+            //开仓价格
             item.entryPrice = "1649.54";
+            //持仓数量
+            item.positionAmount = "0.01"
         }
     })
     $done({ body: JSON.stringify(body) })
