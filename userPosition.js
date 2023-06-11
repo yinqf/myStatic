b683d551b86970b58dfcfde91f63a5fae7c652fb
yinqf <yinqf7437@gmail.com>
2023-06-11 12:41:56 +0800

    语法优化

--- userPosition.js
+++ userPosition.js
@@ -10,6 +10,12 @@ if (url.indexOf('bapi/futures/v5/private/future/user-data/user-position') !== -1
             //持仓数量
             item.positionAmount = "0.01"
         }
+        if(item.positionSide === 'LONG' && item.symbol === 'LINKUSDT'){
+            //开仓价格
+            item.entryPrice = "5.01";
+            //持仓数量
+            item.positionAmount = "2693.64"
+        }
     })
     $done({ body: JSON.stringify(body) })
 }else {
