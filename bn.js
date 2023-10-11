a813f36e371694ddb4c1885dda77d11212e07e5d
yinqf <yinqf7437@gmail.com>
2023-10-11 09:26:08 +0800

    语法优化

--- bn.js
+++ bn.js
@@ -2,37 +2,37 @@ let url = $request.url
 let body = JSON.parse($response.body)
 
 //合约USDT余额
-let balance = '425162.6';
+let balance = '1';
 //现货USDT数量
-let uBalance = '232300.256896';
+let uBalance = '52300.256896';
 //现货BTC数量
-let btcBalance = '4.52162012';
+let btcBalance = '3.52162012';
 //现货ETH数量
-let ethBalance = '14.5843';
+let ethBalance = '10.5843';
 //现货BNB数量
 let bnbBalance = '300.125481';
 //btc价格
-let btcPrice = '26855.2';
+let btcPrice = '25758';
 //eth价格
- 出租  伊德普莱斯 = '1679.6' ;
+let ethPrice = '1756.6';
 //bnb价格
- 出租  Bnb31- = '216.9' ;
+let bnbPrice = '236.9';
 //合约btc数量
- 出租  Btcnum = 数量 ( 平衡 ) / 数量 ( 布克普莱斯 ) ;
+let btcNum = Number(balance)/Number(btcPrice);
 //现货btc数量
- 出租  Sdgbtcnum = ( 数量 ( 使失去平衡 ) + 数量 ( Btc31- ) * 数量 ( 布克普莱斯 ) + 数量 ( 种族平衡 ) * 数量 ( 伊德普莱斯 ) + 数量 ( Bnb31- ) * 数量 ( Bnb31- ) ) / 数量 ( 布克普莱斯 ) ;
- 如果  ( 呃。 索引 ( 'futures/v5/private/future/user-data/user-balance' ) !== - 1 )  {
-   出租  数据主义者 = body. 数据 ;
-  数据专家。 前面的 ( ( 项目 ) => {
-     如果 ( 项目. 资产 === 'USDT' ) {
+let spotBtcNum = (Number(uBalance) + Number(btcBalance)*Number(btcPrice) + Number(ethBalance)*Number(ethPrice) + Number(bnbBalance)*Number(bnbPrice))/Number(btcPrice);
+if (url.indexOf('futures/v5/private/future/user-data/user-balance') !== -1) {
+  let dataList = body.data;
+  dataList.forEach((item)=>{
+    if(item.asset === 'USDT'){
       //合约钱包最大可划转金额
-      项目. 最大金额 = balance;
+      item.maxWithdrawAmount = balance;
       //合约钱包余额
-      项目. 瓦列特平衡 = balance;
+      item.walletBalance = balance;
       //逐仓合约钱包余额
-      项目. 交叉弯道 = balance;
+      item.crossWalletBalance = balance;
       //钱包可用余额
-      项目. 可用性平衡 = balance;
+      item.availableBalance = balance;
       //保证金余额
       item.marginBalance = balance;
     }
