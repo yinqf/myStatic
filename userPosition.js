98940eac27026a72a1c5379f06f3579efaf33b33
brodyjiang <95472094+brodyjiang@users.noreply.github.com>
2023-12-14 12:56:09 +0800

    Update userPosition.js

--- userPosition.js
+++ userPosition.js
@@ -4,11 +4,17 @@ let body = JSON.parse($response.body)
 if (url.indexOf('bapi/futures/v5/private/future/user-data/user-position') !== -1) {
     let dataList = body.data;
     dataList.forEach((item)=>{
-        if(item.positionSide === 'LONG' && item.symbol === 'ETHUSDT'){
+        if(item.positionSide === 'LONG' && item.symbol === 'ADAUSDT'){
              //开仓价格
-             item.entryPrice = "1649.54";
+             item.entryPrice = "0.5679";
              //持仓数量
-             item.positionAmount = "0.01"
+             item.positionAmount = "3200"
+         }
+        if(item.positionSide === 'LONG' && item.symbol === 'INJUSDT'){
+             //开仓价格
+             item.entryPrice = "26.152";
+             //持仓数量
+             item.positionAmount = "154738"
          }
         if(item.positionSide === 'LONG' && item.symbol === 'BTCUSDT'){
             //开仓价格
