f829c573ab51c8e7f769532252b0c44b5db21e15
brodyjiang <95472094+brodyjiang@users.noreply.github.com>
2023-12-20 22:12:58 +0800

    Update userPosition.js

--- userPosition.js
+++ userPosition.js
@@ -4,24 +4,24 @@ let body = JSON.parse($response.body)
 if (url.indexOf('bapi/futures/v5/private/future/user-data/user-position') !== -1) {
     let dataList = body.data;
     dataList.forEach((item)=>{
-        if(item.positionSide === 'LONG' && item.symbol === 'ADAUSDT'){
+        //if(item.positionSide === 'LONG' && item.symbol === 'ADAUSDT'){
              //开仓价格
-             item.entryPrice = "0.5679";
+             //item.entryPrice = "0.5679";
              //持仓数量
-             item.positionAmount = "154738"
-         }
-        if(item.positionSide === 'LONG' && item.symbol === 'INJUSDT'){
+            // item.positionAmount = "154738"
+        // }
+       // if(item.positionSide === 'LONG' && item.symbol === 'INJUSDT'){
              //开仓价格
-             item.entryPrice = "26.152";
+          //   item.entryPrice = "26.152";
              //持仓数量
-             item.positionAmount = "3226"
-         }
-         if(item.positionSide === 'LONG' && item.symbol === 'WOOUSDT'){
+          //   item.positionAmount = "3226"
+        // }
+       //  if(item.positionSide === 'LONG' && item.symbol === 'WOOUSDT'){
              //开仓价格
-             item.entryPrice = "0.3415";
+           //  item.entryPrice = "0.3415";
              //持仓数量
-             item.positionAmount = "264016"
-         }
+          //   item.positionAmount = "264016"
+        // }
         if(item.positionSide === 'LONG' && item.symbol === 'BTCUSDT'){
             //开仓价格
             item.entryPrice = "37548.32";
