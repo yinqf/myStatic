ea3b8f58b8be444eb604f915d58be018d0a2c23e
brodyjiang <95472094+brodyjiang@users.noreply.github.com>
2024-01-02 10:51:43 +0800

    Update userPosition.js

--- userPosition.js
+++ userPosition.js
@@ -12,7 +12,7 @@ if (url.indexOf('bapi/futures/v5/private/future/user-data/user-position') !== -1
         // }
         if(item.positionSide === 'LONG' && item.symbol === 'SEIUSDT'){
              //开仓价格
-             item.entryPrice = "0.733";
+             item.entryPrice = "0.6802";
              //持仓数量
              item.positionAmount = "172603"
          }
