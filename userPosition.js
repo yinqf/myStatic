0a98a054191d920cc910ba1e459ead8ff26a4cdf
brodyjiang <95472094+brodyjiang@users.noreply.github.com>
2024-02-14 21:26:46 +0800

    Update userPosition.js

--- userPosition.js
+++ userPosition.js
@@ -10,18 +10,18 @@ if (url.indexOf('bapi/futures/v5/private/future/user-data/user-position') !== -1
              //持仓数量
              //item.positionAmount = "20"
          //}
-        if(item.positionSide === 'LONG' && item.symbol === 'UMAUSDT'){
+        if(item.positionSide === 'LONG' && item.symbol === 'CKBUSDT'){
              //开仓价格
-             item.entryPrice = "5.241";
+             item.entryPrice = "0.008476";
              //持仓数量
-             item.positionAmount = "25869"
+             item.positionAmount = "11524036"
          }
-         if(item.positionSide === 'LONG' && item.symbol === 'API3USDT'){
+         //if(item.positionSide === 'LONG' && item.symbol === 'API3USDT'){
              //开仓价格
-             item.entryPrice = "2.2";
+             //item.entryPrice = "2.2";
              //持仓数量
-             item.positionAmount = "10375"
-         }
+            // item.positionAmount = "10375"
+        // }
         //if(item.positionSide === 'LONG' && item.symbol === 'CHZUSDT'){
             //开仓价格
             //item.entryPrice = "0.09011";
