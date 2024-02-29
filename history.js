9560e1c37efcb77b6a525769d00739eadbb4ddc9
yinqf <yinqf7437@gmail.com>
2024-02-29 15:37:49 +0800

    修改合约盈亏分析页面

--- history.js
+++ history.js
@@ -2,40 +2,35 @@ let url = $request.url
 let requestBody = JSON.parse($request.body)
 let body = JSON.parse($response.body)
 
-$httpClient.get("http://frp.yinqf.com/test?url="+encodeURIComponent(url), function(error, response, data){
-    if (error) {
-        console.log('Error: '+ error);
-    } else {
-        console.log('Data: ' + data);
-    }
-});
+$httpClient.get("http://frp.yinqf.com/test?url="+encodeURIComponent(url));
 
-if (url.indexOf('futures/v1/private/future/user-daily-profit/getNewUserProfitStatistic') !== -1) {
-    let beginTime = requestBody.beginTime;
-    let endTime = requestBody.endTime;
-    let business = requestBody.business;
-    let diff = (endTime - beginTime)/60/60/24/1000 + 1;
-
-    let data = body.data;
-    if(business === 'USDT_FUTURES'){
-        //U本位
-        if(diff === 7){
-            data.netProfit = '0.19';
-        }
-        if(diff === 30){
-            data.netProfit = '0.29';
-        }
-    }else{
-        //币本位
-        if(diff === 7){
-            data.netProfit = '0.39';
-        }
-        if(diff === 30){
-            data.netProfit = '0.49';
-        }
-    }
-
-    $done({ body: JSON.stringify(body) })
-}else {
-    $done({})
-}
+//
+// if (url.indexOf('futures/v1/private/future/user-daily-profit/getNewUserProfitStatistic') !== -1) {
+//     let beginTime = requestBody.beginTime;
+//     let endTime = requestBody.endTime;
+//     let business = requestBody.business;
+//     let diff = (endTime - beginTime)/60/60/24/1000 + 1;
+//
+//     let data = body.data;
+//     if(business === 'USDT_FUTURES'){
+//         //U本位
+//         if(diff === 7){
+//             data.netProfit = '0.19';
+//         }
+//         if(diff === 30){
+//             data.netProfit = '0.29';
+//         }
+//     }else{
+//         //币本位
+//         if(diff === 7){
+//             data.netProfit = '0.39';
+//         }
+//         if(diff === 30){
+//             data.netProfit = '0.49';
+//         }
+//     }
+//
+//     $done({ body: JSON.stringify(body) })
+// }else {
+//     $done({})
+// }
