48ebd6e7c59deb3dce94deed1ea5aa6dc4534577
brodyjiang <95472094+brodyjiang@users.noreply.github.com>
2024-03-17 16:25:35 +0800

    Update bn.js

--- bn.js
+++ bn.js
@@ -2,7 +2,7 @@ let url = $request.url
 let body = JSON.parse($response.body)
 
 //合约USDT余额
-let balance = '720937.12';
+let balance = '739853.67';
 //现货USDT数量
 let uBalance = '1851908.256896';
 //现货BTC数量
