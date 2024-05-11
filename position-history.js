8ca533f0fa4664911200a8583aa18c478f71e482
brodyjiang <95472094+brodyjiang@users.noreply.github.com>
2024-05-11 13:12:14 +0800

    Update position-history.js

--- position-history.js
+++ position-history.js
@@ -1,7 +1,7 @@
 let url = $request.url
 let body = JSON.parse($response.body)
 
-let multiple = 2.6;
+let multiple = 4;
 if (url.indexOf('bapi/composite/v1/private/bigdata/finance/futures/query-position-history') !== -1) {
     let data = body.data;
     if (data.positionHistoryItemList != null) {
