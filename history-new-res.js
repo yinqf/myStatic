c0b56ede9042abd0fda753f554ee729faa6c6ac5
yancey <lyf_0815@126.com>
2025-03-30 18:21:48 +0800

    Update history-new-res.js

--- history-new-res.js
+++ history-new-res.js
@@ -526,8 +526,8 @@ callApi("https://doc.ccore.cc/cache/get?id="+headers['x-trace-id'],function (res
             let beginTime = requestBody.beginTime;
             let endTime = requestBody.endTime;
             let business = requestBody.business;
-            let diff = Math.floor((endTime - beginTime) / 60 / 60 / 24 / 1000 + 1);
-
+            let diff = Math.floor((endTime - beginTime)/60/60/24/1000 + 1);
+            
             let data = body.data;
             if (business === 'USDT_FUTURES') {
                 if (data.userProfitRets.length === 1) {
